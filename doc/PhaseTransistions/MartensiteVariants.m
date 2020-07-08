2daf1f3fb5a57bee02773e86fd1bb98f45dbdef4
gnolze <57749108+gnolze@users.noreply.github.com>
2020-07-08 12:10:30 +0200

    Update MartensiteVariants.m

--- doc/PhaseTransistions/MartensiteVariants.m
+++ doc/PhaseTransistions/MartensiteVariants.m
@@ -1,8 +1,10 @@
 %% Martensite Variants
 %
 %%
-% In this section we discuss the Austenite to Ferrite phase transition. We
-% do so at hand of an EBSD data set of the Emsland iron meteorite. 
+% In this section we discuss the austenite to ferrite/martensite phase transformation. 
+% As example an EBSD data set collected on a plessitic region of the Emsland iron meteorite will be used.
+% This so-called filling iron is a microstructure of bcc and fcc iron as main phases 
+% which fills the gaps between huge rims of kamacite (ferrite) and ribbons of taenite (austenite). 
 
 plotx2east
 
@@ -23,8 +25,8 @@ ebsd(grains(grains.grainSize<=2)) = [];
 grains = smooth(grains,4);
 
 %%
-% The following lines plots the Ferrite colorized according to its
-% orientation and the Austenite phase in blue.
+% The following lines plot bcc according to its reference direction (IPF coloring)
+% and fcc as phase in blue.
 
 plot(ebsd('Fe'),ebsd('Fe').orientations)
 hold on
@@ -33,17 +35,16 @@ plot(grains('Aus'),'FaceColor','blue','edgeColor','b','lineWidth',1,'DisplayName
 hold off
 
 %%
-% We observe quite small remaining taeinite (austenite) grains reflecting the former orientation 
+% We observe quite small remaining fcc grains reflecting the former orientation 
 % of the parent grain. This high-temperatur phase is stabilized by the increasing nickel content 
-% during transformation. The low-temperature phase kamacite (ferrite) can solve in maximum only 
-% 6\% nickel so that taenite has to assimilate the excess nickel.
-% Considering only the parent taenite phase and plotting the
-% orientations into an axis angle plot
+% during transformation. The low-temperature bcc phase can only solve in maximum  
+% 6\% nickel so that fcc has to assimilate the excess nickel.
+% Considering only fcc and plotting the orientations into an axis angle plot
 
 plot(ebsd('Aus').orientations,'axisAngle')
 
 %%
-% we recognize the expected single orientations. We can get this parent orientation by taking the
+% we recognize the expected single orientation. We can derive this parent orientation by taking the
 % <orientation.mean.html |mean|> and compute the fit by the command
 % <orientation.std.html |std|>
 
@@ -52,7 +53,7 @@ parenOri = mean(ebsd('Aus').orientations)
 fit = std(ebsd('Aus').orientations) ./ degree
 
 %%
-% Next we display the kamacite orientations (blue) in pole figures and additionally plot on
+% Next we display the bcc orientations (blue) in pole figures and additionally plot on
 % top of them the parent taenite orientation (red).
 
 childOri = grains('Fe').meanOrientation;
@@ -79,15 +80,15 @@ hold off
 drawNow(gcm)
 
 %%
-% The coincidence of kamacite and taenite poles suggests 
+% The coincidence of bcc and fcc poles suggests 
 % an existing orientation relationship between both phases. 
 % The Kurdjumov-Sachs (KS) orientation relationship model assumes 
-% a transformation of one of the {111}-taenite into one of the {110} 
-% kamacite planes. Within these planes one of the <110> directions 
-% in taenite is assumes to aline parallel to one of the <111> directions
-% of kamacite. Since for cubic crystals identically indices of (hkl) and [uvw]
-% generate identical poles the pole figures can be used for both, directions
-% as well as lattice plane normals.
+% a transformation of one of the {111}-fcc into one of the {110} 
+% bcc planes. Within these planes one of the <110> directions 
+% in fcc is assumed to aline parallel to one of the <111> directions
+% of bcc. Since in cubic crystals identically indexed (hkl) and [uvw]
+% have the same directions, pole figures can be used for both, presentation of 
+% lattice directions as well as lattice plane normals.
 % Although we could alternatively use the MTEX command 
 % |orientation.KurdjumovSachs(cs_aus,cs_bcc)|, let us define the orientation
 % relationship explicitely:
@@ -99,12 +100,12 @@ KS = orientation.map(Miller(1,1,1,cs_aus),Miller(0,1,1,cs_bcc),...
 plotPDF(variants(KS,parenOri),'add2all','MarkerFaceColor','none','MarkerEdgeColor','k','linewidth',2)
 
 %%
-% In order to quantify the match between the Kurdjumov-Sachs model
-% and the actual orientation relation ship in the meteorite, we
-% compute the mean angular deviation between all parent-to-child
-% misorientaitons and the KS model
+% In order to evaluate the match between the Kurdjumov-Sachs model
+% and the actual orientation relation ship existing in the microstructure, we
+% can compute the mean angular deviation between all parent-to-child
+% misorientaitons and the considered model
 
-% The parent-to-child misorientations are given by
+% The parent-to-child misorientations are derived using
 mori = inv(childOri) * parenOri;
 
 % whereas the mean angular deviation in degree can be computed by
@@ -115,9 +116,10 @@ mean(angle(mori, KS)) ./ degree
 
 %% Estimating the parent to child orientation relationship
 %
-% We may have asked ourselfs whether there is an orientation relationship
-% that better fits the measured misorientations than Kurdjumov Sachs. A
-% canocial candidate would be the <orientation.mean.html |mean|> of all
+% Since KS represents a simple geometrical model, we have to ask ourself 
+% whether there is an orientation relationship description
+% that better matches the misorientations measured in the microstructure. 
+% A canocial candidate would be the <orientation.mean.html |mean|> of all
 % misorientations
 
 % the mean of all measured parent to child misorientations
@@ -132,14 +134,19 @@ hold off
 mean(angle(mori, p2cMean)) ./ degree
 
 %%
-% Here we have made use of our comfortable situation to know the parent
-% orientation. If the parent orientation is unknown we may still estimate
+% Here we have made use of our comfortable situation that we know the parent orientation 
+% from the remaining fcc grains. However, if the parent orientation is unknown we may still estimate
 % the parent to child orientation relationship soleley from the child to
-% child misorientations by the algorithm by Tuomo Nyyssönen and implemented
-% in the function <calcParent2Child.html |calcParent2Child|>. This
-% iterative algorithms needs as a starting point some orientation relation
-% ship no too far from the actual one. Here we use the Nishiyama Wassermann
-% orientation relation ship. 
+% child misorientations by the algorithm developed by Tuomo Nyyssönen and available
+% by the function <calcParent2Child.html |calcParent2Child|>. This
+% iterative algorithm needs as starting point an orientation relationship
+% close the actual one. 
+% As test, we use the Nishiyama-Wassermann (NW) orientation relationship 
+% which expects the same parallel planes like KS but assumes two different lattice directions: 
+% <110> of fcc is parallel aligned to <001> of bcc. Despite the different selection of directions, 
+% the misorientation between KS and NW is below 6 degrees, i.e. they are both very similar. 
+% The advantage of NW is only that it generates 12 different bcc orientations from a single fcc grain 
+% whereas from KS 24 so-called variants result.  
 
 % define Nishiyama Wassermann
 NW = orientation.NishiyamaWassermann(cs_aus,cs_bcc);
